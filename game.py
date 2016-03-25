52b9459e83a81c186de55d847002c9f322ad2488
Ethan <riley787cmplex@gmail.com>
2016-03-25 23:25:36 +0000

    do battle code

--- game.py
+++ game.py
@@ -1,4 +1,21 @@
 import game_data, game_objects 
+import threading
+
+def concat_dicts(*dicts, **kwargs):
+    output = {}
+    for dic in dicts:
+        if len(dic) > 0:
+            keys = output.keys() | dic.keys()
+            output = {k: output.get(k, kwargs["default"]) + dic.get(k, kwargs["default"]) for k in keys }
+    return output
+
+def deconcat_dicts(*dicts): 
+    output = {}
+    for dic in dicts:
+        if len(dic) > 0:
+            keys = output.keys() | dic.keys()
+            output = {k: output.get(k, kwargs["default"]) - dic.get(k, kwargs["default"]) for k in keys }
+    return output
 
 class Game(object):
 
@@ -34,23 +51,43 @@ class entity(meta_sprite):
         self.__equipped = {} 
         self.__inventory = {}
         self.__moves = {}
+        self.__temp_buffs = {}
+        self.__stats = {}
+        self.__stats_needs_update = True 
 
+    def get_buffs_from_dict(self, dictbuff):
+        buffs = []
+        for item in dictbuff:
+            buffs.append(item["buffs"])
+        return concat_dicts(*buffs, default = 0)
+      
     def get_basestats(self):
         return self.__basestat
+   
+    def __update_stats(self):
+        self.__stats = concat_dicts(self.__basestat, 
+                self.get_buffs_from_dict(self.__temp_buffs), 
+                    self.get_buffs_from_dict(self.__equipped), default = 0) 
 
+    def get_stats(self):
+        if self.__stats_needs_update:
+            self.__update_stats()
+            self.__stats_needs_update = False
+        return self.__stats
+    
     def add_move(self, move):
         self.__moves[move["name"]] = move
 
     def get_moves(self):
-        return self.__move
+        return self.__moves
 
     def unequip(itemname):
-        self.inventory[itemname] = self.__equppped[itemname]
-        for buff in self.inventory[itemname]:
-            self.basestat[buff] -= self.__inventory[itemname]["buffs"][buff] * self.inventory[itemname]["potency"]
+        self.__stats_needs_update =  True
+        self.inventory[itemname] = self.__equipped[itemname]
         self.__equipped.pop(itemname)
 
     def equip(itemname):
+        self.__stats_needs_update =  True
         self.__equipped[itemname] = self.__inventory[itemname]
         self.__inventory.pop(itemname)
         
@@ -70,6 +107,7 @@ class entity(meta_sprite):
         self.add_to_inventory(item1)
 	
     def use_item(self, itemname, game):
+        self.__stats_needs_update =  True
         for buff in self.__inventory[itemname]["buffs"]:
            self.basestat[buff] +=  self.__inventory[itemname]["buffs"][buff] * self.inventory[itemname]["potency"]
         if self.__inventory[itemname][effect] != None:
@@ -80,6 +118,7 @@ class entity(meta_sprite):
             self.remove_from_inventory(self.__inventory[itemname])
 
     def use_move(self, movename, game):
+        self.__stats_needs_update =  True
         for buff in self.moves[movename]["buffs"]:
             self.basestat += self.moves[movename]["buffs"][buffs]
         self.temp_buffs.append(self.moves[movename]["temporary_buffs"])
@@ -97,32 +136,43 @@ class player(entity):
     def is_player(self):
         return True;
 			
-class battle_entity(Thread):
+class battle_entity:
    
-    def __init__(self, entity):
+    def __init__(self, entity, game):
+        entity.load_battle_assets(game)
         self.__entity = entity
-        self.__func_queue = queue.Queue()
+        self.__func_queue = collections.Deque()
 
     def use_item(self, itemname):
-        self.__entity.use_item(itemname)
+        self.__func_queue.append({"func" : self.__entity.use_item,"args" : itemname})
 
     def use_move(self, movename):
-        self.__entity.use_move(movename)
+        self.__func_queue.append({"func" : self.__entity.use_move, "args" : movename})
 
-    def get_basestats(self):
-        return self.__basestat
+    def do_queued(self):
+            function = self.queue.pop()
+            function["func"]("args")
+            self.turns += 1
 
-class battle:
+    def ready(self):
+        if len(self.__func_queue) > 0:
+            return True
+        return False
+
+    def get_stats(self):
+        return self.__entity.get_stats()
+
+class battle(threading.Thread):
     def __init__(self, entity, entity1, game):
-            entity.load_battle_assets(entity1, game)
+            threading.Thread.__init__(self)
+            self.entity = battle_entity(entity, game)
+            self.entity1 = battle_entity(entity1, game)
             game.set_background("battle")   
-            entity1.load_battle_assets(entity, game)
-            entity_queuefuncs = []
-            entity1_queuefuncs = []
-            if entity.get_basestats()["speed"] < entity1.get_basestats()["speed"]:
-                self.entity_turn = False
-            else:
-                self.entity_turn = True
+            self.start()
 
-    def entity_do(self, entityNo, do_func):
-            self.entity_queuefuncs.append(do_func)
+    def run(self): 
+        if self.__entity_turn & self.entity.ready():
+            self.entity.do_queued()
+        if not self.__entity_turn & self.entity1.ready():
+            self.entity1.do_queued()
+
